0f0f12927ab96ac698a354a20cc8571d81d41b31
jjhales <jjhales@fullsail.edu>
2015-06-23 22:04:02 -0400

    adding version 2.0 to gh-pages
    Version 2.0 with working code and video links to working code

---
+++ v2.0/js/main.js
@@ -0,0 +1,30 @@
+function bc_vid(){
+	document.getElementById("bc_video").style.display="inline";
+}
+function bc_v_hide(){
+	document.getElementById("bc_video").style.display="none";
+}
+function fv_vid(){
+	document.getElementById("fruit_videos").style.display="inline";
+}
+function fv_hide(){
+	document.getElementById("fruit_videos").style.display="none";
+};
+function htm_vid(){
+	document.getElementById("htm_videos").style.display="inline";
+}
+function htm_hide(){
+	document.getElementById("htm_videos").style.display="none";
+};
+function ccm_vid(){
+	document.getElementById("ccm_videos").style.display="inline";
+}
+function ccm_hide(){
+	document.getElementById("ccm_videos").style.display="none";
+}
+function ht_vid(){
+	document.getElementById("ht_videos").style.display="inline";
+}
+function ht_hide(){
+	document.getElementById("ht_videos").style.display="none";
+};
